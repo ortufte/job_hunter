60f6ec5982a372fd313efec483241eaf24c86c99
github username <github email address>
2020-04-08 17:07:08 -0500

    build out new, create, edit, update, and delete actions in opportunities controller

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,2 +1,42 @@
 class OpportunitiesController < ApplicationController
+
+    def new
+        @opportunity = Opportunity.new
+    end
+
+    def create
+        @opportunity = current_user.opportunities.build(opportunity_params)
+        if @opportunity.save
+            redirect_to user_path(current_user)
+        else
+            redirect_to new_user_opportunity_path, :notice => "Oops, something went wrong. Please try again."
+        end
+    end
+
+    def edit
+        @opportunity = Opportunity.find_by(:id => params[:id])
+    end
+
+    def update
+        @opportunity = Opportunity.find_by(:id => params[:id])
+        if @opportunity.update(opportunity_params)
+            redirect_to user_path(current_user)
+        else
+            render :edit, :notice => "Oops, something went wrong. Please try again."
+        end
+    end
+
+    def destroy
+        @opportunity = Opportunity.find_by(:id => params[:id])
+        @opportunity.delete
+        redirect_to user_path(current_user)
+    end
+
+    private
+
+    def opportunity_params
+        params.require(:opportunity).permit(:title, :company, :location, :contact, :phone, :email, :job_listing, :closed, :user_id)
+    end
+
+
 end
