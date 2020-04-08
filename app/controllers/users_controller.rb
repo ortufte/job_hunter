1b032d241a716a2b1f9bde4e8d09811f6b07f3e8
github username <github email address>
2020-04-08 11:07:53 -0500

    revise redirect routes to use restful routing in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            redirect_to '/home'
+            redirect_to user_path(user)
         else
-            redirect_to '/signup'
+            redirect_to new_user_path
         end
     end
 
