6fe00c81e9920f7cc08dd05cdad83a3fc7428ebe
github username <github email address>
2020-04-08 11:07:17 -0500

    revise redirect routes to use restful routing in sessionns controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,19 @@ class SessionsController < ApplicationController
     def new
     end
 
-    def login
-        @user = User.find_by(:email => params[:email])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            render '/users/show'
+    def create
+        user = User.find_by(:email => params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
         else
-            redirect_to '/login', :notice => "User not found, please try again."
+            redirect_to new_session_path, :notice => "User not found, please try again."
         end
     end
 
-    def logout
+    def destroy
         reset_session
+        redirect_to root_path
     end
     
 end
