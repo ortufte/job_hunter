526a963cc0b8627099937afc1f35107e4763dd53
github username <github email address>
2020-04-08 11:12:27 -0500

    revise user and session routes to use restful routing and add qualification routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'application#index'
 
-  get '/signup', to: 'users#new'
+  resources :users, only: [:new, :create, :show] 
 
-  post '/users', to: 'users#create'
+  resources :sessions, only: [:new, :create, :destroy]
 
-  get '/home', to: 'users#show'
-
-  get '/login', to: 'sessions#new'
-
-  post '/sessions', to: 'sessions#login'
-
-  get '/logout', to: 'sessions#logout'
+  resources :qualifications
 
 end
