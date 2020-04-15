17d8359d5c31a71d09afd82d686ca5bb89236af2
github username <github email address>
2020-04-15 15:07:10 -0500

    set session in new action, add edit and update actions, update user_params to permit qualification ids and nested attributes for qualifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
+            session[:user_id] = user.id
             redirect_to user_path(user)
         else
             redirect_to new_user_path
@@ -17,10 +18,24 @@ class UsersController < ApplicationController
         @user = User.find_by(:id => params[:id])
     end
 
+    def edit
+        @user = User.find_by(:id => params[:id])
+    end
+
+    def update
+        @user = User.find_by(:id => params[:id])
+        if @user.update(:qualification_ids => user_params[:qualification_ids])
+            redirect_to user_user_qualifications_path(current_user)
+        else
+            redirect_to user_user_qualifications_path(current_user), :notice => "Oops, something went wrong. Please try again."
+        end
+    end
+
+
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :qualification_ids => [ ], :qualification_attributes => [:description])
     end
 
 end
