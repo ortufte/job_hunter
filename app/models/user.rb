db623e54fb553fc135fdeb7432154038aa059db7
github username <github email address>
2020-04-15 15:08:32 -0500

    update user validations to only run on password and password confirmation on the create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
     has_secure_password
+
     has_many :opportunities
     has_many :user_qualifications
     has_many :qualifications, through: :user_qualifications
 
-    validates :name, :email, :password, presence: true
+    validates :name, :email, :password, presence: true, :on => :create
     validates :password, confirmation: true
-    validates :password_confirmation, presence: true
+    validates :password_confirmation, presence: true, :on => :create
 
 end
