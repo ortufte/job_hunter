1f8359e23c9b7cd0ec4c2e4a5f66ac8cd8fa93ea
github username <github email address>
2020-04-21 09:29:11 -0500

    create opportunity_qualifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_135046) do
+ActiveRecord::Schema.define(version: 2020_04_15_192823) do
 
   create_table "opportunities", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2020_04_09_135046) do
     t.string "job_listing"
     t.boolean "closed", default: false
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "opportunity_qualifications", force: :cascade do |t|
+    t.integer "opportunity_id"
+    t.integer "qualification_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
