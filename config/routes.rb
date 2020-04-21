4e161b0838da111c3c36e673ae8d8424afd8797b
github username <github email address>
2020-04-21 09:28:44 -0500

    remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#index'
 
-  resources :users, only: [:new, :create, :show] do
-    resources :opportunities, only: [:new, :create, :edit, :update, :destroy]
-    resources :user_qualifications, only: [:index, :new, :edit, :create, :update, :destroy]
+  resources :users, only: [:new, :create, :edit, :update, :show] do
+    resources :opportunities, only: [:new, :create, :show, :edit, :update, :destroy]
+    resources :user_qualifications, only: [:index, :edit, :update, :destroy]
   end
 
   resources :sessions, only: [:new, :create, :destroy]
-
-  resources :qualifications, only: [:index, :new, :create]
-
+  
 end
