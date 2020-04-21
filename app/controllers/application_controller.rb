5714e5537edbcf9ffc8d7f77f66b9ed825e9f3ff
github username <github email address>
2020-04-21 09:23:27 -0500

    create logged_in? helper method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
+    helper_method :logged_in?
     
     def index
     end
 
     def current_user
         @current_user ||= User.find_by(:id => session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
     end
     
 end
