f143e55c732ef4e8a83fdac4931972d399b7a504
github username <github email address>
2020-05-05 15:49:40 -0500

    re-add index, edit and update actions to user_qualifications controller

--- app/controllers/user_qualifications_controller.rb
+++ app/controllers/user_qualifications_controller.rb
@@ -1,8 +1,22 @@
 class UserQualificationsController < ApplicationController
 
-    def index
-       
+    def index  
     end
+
+    def edit
+        @user_qualification = UserQualification.find_by(:id => params[:id])
+    end
+
+    def update
+        @user_qualification = UserQualification.find_by(:id => params[:id])
+        if @user_qualification.update(user_qualification_params)
+            redirect_to user_user_qualifications_path
+        else
+            redirect_to edit_user_user_qualification_path, :notice => "Oops, something went wrong. Please try again."
+        end
+    end
+
+
 
     private
 
