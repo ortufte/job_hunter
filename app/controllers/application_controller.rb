b81e9bb5503228a7920aaeb6895179bc8d0fd87e
github username <github email address>
2020-05-12 08:53:00 -0500

    restrict access to logged in users except application index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
+
+    before_action :require_login 
+    skip_before_action :require_login, only: [:index]
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+    end
     
     def index
         @user = User.new
